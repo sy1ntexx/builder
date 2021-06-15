64c92cc617b3cbf6604a8eeb30aec875d7e5fc55
sy1ntexx <sy1ntexx@gmail.com>
2021-06-15 16:09:26 +0300

    Added skip and default field attributes

--- examples/01-test.rs
+++ examples/01-test.rs
@@ -1,24 +1,33 @@
 use builder::Builder;
 
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum Val {
+    Test,
+    Group,
+}
+
 #[derive(Builder)]
 pub struct Resource {
     pub inline: Option<bool>,
-    pub value: u32,
+    #[skip]
+    #[default(Val::Test)]
+    pub value: Val,
     pub r#type: String,
     pub some: (u32, u32),
-    pub r#match: Option<bool>
+    pub r#match: Option<bool>,
 }
 
 fn main() {
     let t: Resource = Resource::builder()
         .inline(false)
-        .with_value(321)
         .with_type(String::from("Test"))
         .with_some((3, 5))
+        .r#match(true)
         .build();
 
-    assert_eq!(t.value, 321);
     assert_eq!(t.some, (3, 5));
+    assert_eq!(t.value, Val::Test);
     assert_eq!(t.inline, Some(false));
+    assert_eq!(t.r#match, Some(true));
     assert_eq!(t.r#type, String::from("Test"));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
-use syn::*;
-use quote::*;
 use proc_macro::TokenStream;
+use quote::*;
+use syn::__private::TokenStream2;
+use syn::*;
 
 fn fix_raw_ident(ident: Ident) -> Ident {
     let s = ident.to_string();
-    if s.contains("#") {
+    if s.contains('#') {
         Ident::new(s.split_once('#').unwrap().1, ident.span())
     } else {
         ident
@@ -32,7 +33,9 @@ fn is_type_of(ty: &Type, target: &str) -> bool {
 fn get_type_under_option(f: &Field) -> Option<&Type> {
     if is_field_type_of(f, "Option") {
         if let Type::Path(ref tp) = f.ty {
-            if let PathArguments::AngleBracketed(ref ag) = tp.path.segments.first().unwrap().arguments {
+            if let PathArguments::AngleBracketed(ref ag) =
+                tp.path.segments.first().unwrap().arguments
+            {
                 if let GenericArgument::Type(ref ty) = ag.args.first().unwrap() {
                     return Some(ty);
                 }
@@ -42,9 +45,91 @@ fn get_type_under_option(f: &Field) -> Option<&Type> {
     None
 }
 
-#[proc_macro_derive(Builder)]
+fn get_fields(nf: &FieldsNamed) -> impl Iterator<Item = TokenStream2> + '_ {
+    nf.named.iter().map(|f| {
+        let ident = f.ident.as_ref().unwrap();
+        let ty = &f.ty;
+        if is_field_type_of(f, "Option") {
+            quote! { #ident: #ty, }
+        } else {
+            quote! { #ident: ::core::option::Option<#ty>, }
+        }
+    })
+}
+
+fn get_methods(nf: &FieldsNamed) -> impl Iterator<Item = TokenStream2> + '_ {
+    nf.named
+        .iter()
+        .cloned()
+        .map(|f| {
+            if is_field_type_of(&f, "bool") || {
+                if let Some(ty) = get_type_under_option(&f) {
+                    is_type_of(ty, "bool")
+                } else {
+                    false
+                }
+            } {
+                f.ident.unwrap()
+            } else {
+                format_ident!("with_{}", fix_raw_ident(f.ident.unwrap()).to_string())
+            }
+        })
+        .zip(nf.named.iter().cloned())
+        .map(|e| {
+            if to_skip(&e.1) {
+                quote! {}
+            } else {
+                let func_name = e.0;
+                let field_name = e.1.ident.as_ref().unwrap();
+                let field_type = if let Some(ty) = get_type_under_option(&e.1) {
+                    ty
+                } else {
+                    &e.1.ty
+                };
+                quote! {
+                    #[inline]
+                    pub fn #func_name(mut self, mut #field_name: #field_type) -> Self {
+                        self.#field_name = ::core::option::Option::Some(#field_name);
+                        self
+                    }
+                }
+            }
+        })
+}
+
+fn to_skip(f: &Field) -> bool {
+    f.attrs.iter().any(|a| {
+        a.path
+            .segments
+            .first()
+            .unwrap()
+            .ident
+            .to_string()
+            .contains("skip")
+    })
+}
+
+fn get_default_value(f: &Field) -> TokenStream2 {
+    if let Some(a) = f.attrs.iter().find(|a| {
+        a.path
+            .segments
+            .first()
+            .unwrap()
+            .ident
+            .to_string()
+            .contains("default")
+    }) {
+        let tokens: TokenStream = a.clone().tokens.into();
+        let value: Expr = parse(tokens).unwrap();
+        return quote! { ::core::option::Option::Some(#value) };
+    }
+    return quote! { ::core::default::Default::default() };
+}
+
+#[proc_macro_derive(Builder, attributes(skip, default))]
 pub fn test(ts: TokenStream) -> TokenStream {
     let input = parse_macro_input!(ts as DeriveInput);
+    //eprintln!("{:#?}", input);
     let in_ident = &input.ident;
     let b_name = format_ident!("{}Builder", input.ident.to_string());
 
@@ -59,65 +144,41 @@ pub fn test(ts: TokenStream) -> TokenStream {
         panic!("You can only apply builder pattern to struct");
     }
 
-    let fields = nf.named.iter().map(
-        |f| {
-            let ident = f.ident.as_ref().unwrap();
-            let ty = &f.ty;
-            if is_field_type_of(f, "Option") {
-                quote! { #ident: #ty, }
-            } else {
-                quote! { #ident: std::option::Option<#ty>, }
-            }
-        }
-    );
-    let methods = nf.named.iter().cloned().map(
-        |f| {
-            if is_field_type_of(&f, "bool") || { if let Some(ty) = get_type_under_option(&f) { is_type_of(ty, "bool") } else { false } } {
-                f.ident.unwrap()
-            } else {
-                format_ident!("with_{}", fix_raw_ident(f.ident.unwrap()).to_string())
-            }
-        }
-    ).zip(nf.named.iter().cloned()).map(|e| {
-        let func_name = e.0;
-        let field_name = e.1.ident.as_ref().unwrap();
-        let field_type = if let Some(ty) = get_type_under_option(&e.1) { ty } else { &e.1.ty };
-        quote! {
-            #[inline]
-            pub fn #func_name(mut self, mut #field_name: #field_type) -> Self {
-                self.#field_name = std::option::Option::Some(#field_name);
-                self
-            }
-        }
-    });
-
     let generics = input.generics.params;
     let where_clause = input.generics.where_clause;
 
-    let assign = nf.named.iter().map(
-        |f| {
-            let name = f.ident.as_ref().unwrap();
-            let expect_message = format!("{} cannot be None.", name.to_string());
-            let value = if is_field_type_of(f, "Option") {
-                quote! { self.#name }
-            } else {
-                quote! { self.#name.expect(#expect_message) }
-            };
-            quote! {
-                #name: #value,
-            }
+    let assign = nf.named.iter().map(|f| {
+        let name = f.ident.as_ref().unwrap();
+        let expect_message = format!("{} cannot be None.", name.to_string());
+        let value = if is_field_type_of(f, "Option") {
+            quote! { self.#name }
+        } else {
+            quote! { self.#name.expect(#expect_message) }
+        };
+        quote! {
+            #name: #value,
         }
-    );
+    });
 
-    let build = quote! {
+    let assignment = nf.named.iter().map(|f| {
+        let id = f.ident.as_ref().unwrap();
+        let val = get_default_value(f);
+        quote! {
+            #id: #val,
+        }
+    });
+
+    let builder = quote! {
         #[inline]
-        pub fn build(self) -> #in_ident<#generics> #where_clause {
-            #in_ident::<#generics> {
-                #(#assign)*
+        pub fn builder() -> #b_name<#generics> #where_clause {
+            #b_name::<#generics> {
+                #(#assignment)*
             }
         }
     };
 
+    let methods = get_methods(&nf);
+    let fields = get_fields(&nf);
     let out = quote! {
         #[derive(Debug, Clone, Default)]
         pub struct #b_name<#generics> #where_clause {
@@ -125,15 +186,18 @@ pub fn test(ts: TokenStream) -> TokenStream {
         }
 
         impl<#generics> #in_ident<#generics> #where_clause {
-            #[inline]
-            pub fn builder() -> #b_name<#generics> #where_clause {
-                <#b_name<#generics>>::default()
-            }
+            #builder
         }
 
         impl<#generics> #b_name<#generics> #where_clause {
             #(#methods)*
-            #build
+
+            #[inline]
+            pub fn build(self) -> #in_ident<#generics> #where_clause {
+                #in_ident::<#generics> {
+                    #(#assign)*
+                }
+            }
         }
     };
     out.into()
